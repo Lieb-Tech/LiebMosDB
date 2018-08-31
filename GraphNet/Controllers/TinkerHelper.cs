682d37259d96d7029608bca24b95690d1ce125d3
Marten Liebster <mliebster@lieb-tech.com>
2018-08-31 10:42:45 -0400

    Update TinkerHelper.cs

--- GraphNet/Controllers/TinkerHelper.cs
+++ GraphNet/Controllers/TinkerHelper.cs
@@ -7,18 +7,18 @@ using Gremlin.Net.Driver;
 using Newtonsoft.Json;
 
 namespace GraphNet.Controllers
-{
-    public class TinkerHelper
-    {
-        static string endpoint = "https://liebmos.documents.azure.com:443";        
         // static string hostname = "liebmos.gremlin.cosmosdb.azure.com";
         
-        static string authKey = "1DSoCTY32l5O2Ixu5a3knwKx222b2n60gNw07vyv0WLdPfTX2Zjn4fJewxmnJn2s4FmFl3XjjgeavcxfR8RVJg==";
+        static string authKey = "";
 
         static string hostname = "liebmos.gremlin.cosmosdb.azure.com";
         private static string database = "graphdb";
         private static string collection = "Persons2";
         static int port = 443;
+{
+    public class TinkerHelper
+    {
+        static string endpoint = "https://liebmos.documents.azure.com:443";        
            
         static GremlinServer gremlinServer = new GremlinServer(hostname, port, enableSsl: true,
                                                 username: $"/dbs/{database}/colls/{collection}",
